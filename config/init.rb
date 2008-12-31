cbe42efecb3fda29b195b2913867d99300435712
Nacho <nacho@nucleartesuji.com>
2008-12-31 16:08:56 -0200

    /bin/ratings created

--- config/init.rb
+++ config/init.rb
@@ -3,17 +3,12 @@ require 'sequel'
 
 require 'logger'
 
-
-def db_connection
-  Sequel.sqlite(File.dirname(__FILE__) + '/../data/ratings.db', :loggers => [ logger ])
-end
-
 if ENV['ENVIRONMENT'] == 'test'
   logger = Logger.new(File.dirname(__FILE__) + '/../logs/test.log')
   DB = Sequel.sqlite "", :loggers => [ logger ]
 else
   logger = Logger.new(File.dirname(__FILE__) + '/../logs/ratings.log')
-  DB = db_connection
+  DB = Sequel.sqlite(File.dirname(__FILE__) + '/../data/ratings.db', :loggers => [ logger ])
 end
 
 require File.dirname(__FILE__) + '/../lib/ratings.rb'
