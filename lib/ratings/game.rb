e35fdcd1173f8f2c78cf31e32b97aac724579274
Nacho <nacho@nucleartesuji.com>
2008-12-31 14:36:29 -0200

    Rating change calculations

--- lib/ratings/game.rb
+++ lib/ratings/game.rb
@@ -15,6 +15,16 @@ module Ratings
     def winner= v
       @values[:winner] = v.to_s
     end
+
+    def rating_change_for which_player
+      which_opponent = (which_player == :left) ? :right : :left
+      player, opponent = [which_player, which_opponent].map { |p| send(p) }
+      return Parameters::Con(player.rating) * (result(which_player) - Parameters::expected(player.rating, opponent.rating))
+    end
+
+    def result which_player
+      (which_player == winner) ? 1 : 0
+    end
   end
 end
 
--- spec/lib/ratings/game_spec.rb
+++ spec/lib/ratings/game_spec.rb
@@ -14,5 +14,66 @@ describe Ratings::Game do
     game.right.should == @right
     game.winner.should == :left
   end
+
+  describe "ratings calculations" do
+    before do
+      @game = Game.create(:left => @left, :right => @right, :winner => :left)
+    end
+
+    it "should calculate a positive rating change for the winner" do
+      @game.rating_change_for(:left).should > 0
+    end
+
+    it "should calculate a negative rating change for the loser" do
+      @game.rating_change_for(:right).should < 0
+    end
+
+    describe "for same rating" do
+      describe "(2400)" do
+        before do
+          left = Player.create(:name => "left", :rating => 2400)
+          right = Player.create(:name => "right", :rating => 2400)
+          @game = Game.create(:left => left, :right => right, :winner => :left)
+        end
+
+        it "should calculate the correct rating change for the loser" do
+          @game.rating_change_for(:right).should == -7.5
+        end
+        it "should calculate the correct rating change for the winner" do
+          @game.rating_change_for(:left).should == 7.5
+        end
+      end
+      describe "(500)" do
+        before do
+          left = Player.create(:name => "left", :rating => 500)
+          right = Player.create(:name => "right", :rating => 500)
+          @game = Game.create(:left => left, :right => right, :winner => :left)
+        end
+
+        it "should calculate the correct rating change for the loser" do
+          @game.rating_change_for(:right).should == -47.5
+        end
+        it "should calculate the correct rating change for the winner" do
+          @game.rating_change_for(:left).should == 47.5
+        end
+      end
+    end
+
+    describe "for ratings 320, 400" do
+      before do
+        left = Player.create(:name => "left", :rating => 400)
+        right = Player.create(:name => "right", :rating => 320)
+        @game = Game.create(:left => left, :right => right, :winner => :right)
+      end
+
+      it "should calculate the correct rating change for the loser" do
+        @game.rating_change_for(:left).should be_close(-60, 1)
+      end
+      it "should calculate the correct rating change for the winner" do
+        @game.rating_change_for(:right).should be_close(63, 1)
+      end
+    end
+
+  end
 end
 
