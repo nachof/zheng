7ce2881acf9a0e6f3d65095ae7b7279f40070e54
Nacho <nacho@nucleartesuji.com>
2008-12-30 22:03:22 -0200

    Logger to file

--- config/init.rb
+++ config/init.rb
@@ -6,11 +6,13 @@ require 'logger'
 require File.dirname(__FILE__) + '/../lib/ratings.rb'
 
 def db_connection
-  Sequel.sqlite(File.dirname(__FILE__) + '/../data/ratings.db', :loggers => [ Logger.new($stdout) ])
+  Sequel.sqlite(File.dirname(__FILE__) + '/../data/ratings.db', :loggers => [ logger ])
 end
 
 if ENV['ENVIRONMENT'] == 'test'
-  DB = Sequel.sqlite
+  logger = Logger.new(File.dirname(__FILE__) + '/../logs/test.log')
+  DB = Sequel.sqlite "", :loggers => [ logger ]
 else
+  logger = Logger.new(File.dirname(__FILE__) + '/../logs/ratings.log')
   DB = db_connection
 end
