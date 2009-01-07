25b64fbe53c31081b632b95f2a240983846ecc96
Nacho <nacho@nucleartesuji.com>
2009-01-06 23:05:05 -0200

    Load DB config from config file, if present at ~/zheng.yaml

--- config/init.rb
+++ config/init.rb
@@ -1,16 +1,29 @@
 require 'rubygems'
 require 'sequel'
+require 'erb'
 
 require 'shellwords' # Needed for parsing the script action
 
 require 'logger'
+
+Dir.chdir File.join(File.dirname(__FILE__), '..')
+
+def config
+  $__config ||= (
+    user_conf = File.expand_path('~/.zheng.yaml')
+    default_conf = File.join(File.dirname(__FILE__), 'default.yaml')
+    conf_file = File.exist?(user_conf) ? user_conf : default_conf
+    YAML.load(ERB.new(File.read(conf_file)).result) 
+  )
+end
 
 if ENV['ENVIRONMENT'] == 'test'
   logger = Logger.new(File.join(File.dirname(__FILE__), '..', 'logs', 'test.log'))
   DB = Sequel.sqlite "", :loggers => [ logger ]
 else
   logger = Logger.new(File.join(File.dirname(__FILE__), '..', 'logs', 'zheng.log'))
-  DB = Sequel.sqlite(File.join(File.dirname(__FILE__), '..', 'data', 'zheng.db'), :loggers => [ logger ])
+  DB = Sequel.connect(config[:database], :loggers => [ logger ])
 end
+
 
 require File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib', 'zheng'))
