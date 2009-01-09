27283e588928acae1ae4ba216e77db4a65814c36
Nacho <nacho@nucleartesuji.com>
2009-01-09 14:55:02 -0200

    Move the table creation to config/init.rb. Remove util/make_tables.rb.
    Tables are now created automatically if they don't exist.
    Also, spec_helper looks a little cleaner including config/init instead of util/make_tables.

--- config/init.rb
+++ config/init.rb
@@ -26,3 +26,12 @@ else
 end
 
 require File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib', 'zheng'))
+
+# This will create the tables only if they don't already exist.
+# This should be done using Sequel's table_exists? method, but it always returns true.
+begin
+  DB.from(:players).first
+rescue
+  Zheng::Player.create_table
+  Zheng::Game.create_table
+end
