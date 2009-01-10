88684dc8aec7c3eb15349afc85041ec93a66ccfc
Nacho <nacho@nucleartesuji.com>
2009-01-10 19:19:24 -0200

    If user's config file does not exist, create it from default instead of just using default.

--- config/init.rb
+++ config/init.rb
@@ -6,14 +6,16 @@ require 'shellwords' # Needed for parsing the script action
 
 require 'logger'
 
+require 'ftools'
+
 Dir.chdir File.join(File.dirname(__FILE__), '..')
 
 def config
   $__config ||= (
     user_conf = File.expand_path('~/.zheng.yaml')
     default_conf = File.join(File.dirname(__FILE__), 'default.yaml')
-    conf_file = File.exist?(user_conf) ? user_conf : default_conf
-    YAML.load(ERB.new(File.read(conf_file)).result) 
+    File.copy(default_conf, user_conf) unless File.exist? user_conf
+    YAML.load(ERB.new(File.read(user_conf)).result) 
   )
 end
 
@@ -21,8 +23,7 @@ if ENV['ENVIRONMENT'] == 'test'
   logger = Logger.new(File.join(File.dirname(__FILE__), '..', 'logs', 'test.log'))
   DB = Sequel.sqlite "", :loggers => [ logger ]
 else
-  logger = Logger.new(File.join(File.dirname(__FILE__), '..', 'logs', 'zheng.log'))
-  DB = Sequel.connect(config[:database], :loggers => [ logger ])
+  DB = Sequel.connect(config[:database])
 end
 
 require File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib', 'zheng'))
