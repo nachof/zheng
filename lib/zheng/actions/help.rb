1c2920e77ae04466f52e3001facfffac180d996b
Nacho <nacho@nucleartesuji.com>
2009-02-09 12:33:01 -0200

    Help text

--- lib/zheng/actions/help.rb
+++ lib/zheng/actions/help.rb
@@ -10,6 +10,42 @@ module Zheng
         Zheng.output "database"
         Zheng.output "help"
       end
+
+      def database
+        output "Manipulate the database"
+        output ""
+        output "database clear"
+        output "   Clear all data in the database"
+      end
+
+      def game
+        output "Manipulate games"
+        output ""
+        output "game add player1 player2 winner"
+        output "   Add a game between player1 and player2, won by the winner specified"
+        output "   Winner can be 'left' or 'right', or 'first' or 'second'"
+        output "   Example: game add \"Cho Chikun\" \"Takemiya Masaki\" first"
+      end
+
+      def player
+        output "Manipulate players"
+        output ""
+        output "player add name level"
+        output "   Adds a player with the specified starting level"
+        output "   Level can be a ranking or a rating value"
+        output "   Example: player add \"John Doe\" 1k"
+        output "player add_external name level"
+        output "   Like add but adds an external player"
+        output "   External players serve as anchors, and their rating does not vary normally"
+        output "player list"
+        output "   Produce a list of all the non external players"
+        output "player list all"
+        output "   Produce a list of all the players, including external players"
+        output "player set_rating name level"
+        output "   Sets the rating of the player to the specified rating or rank"
+        output "   Level can be a ranking or rating value"
+        output "   Example: player set_rating \"John Doe\" 2d"
+      end
     end
   end
 end
